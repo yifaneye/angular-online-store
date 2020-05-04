7129d4dc9826484e39c27de1c049945476ba5e47
Yifan Ai <ayfallen@hotmail.com>
2020-05-05 00:39:43 +1000

    Complete app and deploy to firebase

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -55,9 +55,15 @@
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
-import 'zone.js/dist/zone';  // Included with Angular CLI.
-
+import 'zone.js/dist/zone'; // Included with Angular CLI.
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
